b11dea37fb8dbea05e132d57d1d13a992cef3bf7
Muhammad Iqbal Assegaf <assegaf.iqbal99@gmail.com>
2022-11-06 14:34:18 +0700

    update button next

--- lib/main.dart
+++ lib/main.dart
@@ -27,56 +27,101 @@ class Onboarding extends StatefulWidget {
   State<Onboarding> createState() => _OnboardingState();
 }
 
-class _OnboardingState extends State<Onboarding> {
+class _OnboardingState extends State<Onboarding>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-    String content = 'aaa';
+
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-              child: PageView(
-            onPageChanged: (value) {
-              content = 'bbb';
-              setState(() {});
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          TextButton(
+            onPressed: () {
+              _controller.forward();
             },
-            children: [
-              Container(
-                height: height,
-                width: width,
-                color: Colors.red,
-              ),
-              Container(
-                height: height,
-                width: width,
-                color: Colors.redAccent,
-              ),
-              Container(
-                height: height,
-                width: width,
+            child: const Text(
+              'Skip',
+              style: TextStyle(
                 color: Colors.purple,
-              ),
-            ],
-          )),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              height: height * 0.5,
-              width: width,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(100),
-                ),
-              ),
-              child: Center(
-                child: Text(content),
+                fontSize: 18,
               ),
             ),
           ),
         ],
+      ),
+      body: PageView(),
+      bottomNavigationBar: SafeArea(
+        minimum: const EdgeInsets.all(15),
+        child: SizedBox(
+          height: kToolbarHeight + 20,
+          width: 10,
+          // decoration: BoxDecoration(
+          //   color: Colors.black,
+          //   borderRadius: BorderRadius.circular(10),
+          // ),
+          child: Center(
+            child: Stack(
+              children: [
+                Positioned(
+                  child: SizedBox.fromSize(
+                    size: const Size(55, 55),
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.transparent,
+                      value: _controller.value,
+                      valueColor:
+                          const AlwaysStoppedAnimation<Color>(Colors.purple),
+                      strokeWidth: 2.5,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  top: 0,
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    padding: const EdgeInsets.all(5),
+                    child: GestureDetector(
+                      onTapDown: (_) {
+                        _controller.value += .25;
+                      },
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.purple,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
