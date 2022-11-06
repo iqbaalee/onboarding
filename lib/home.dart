1b44b7e7974fc103111e863e02dd5e1a11d27649
Muhammad Iqbal Assegaf <assegaf.iqbal99@gmail.com>
2022-11-06 15:23:21 +0700

    add screen page view

--- lib/home.dart
+++ lib/home.dart
@@ -5,17 +5,36 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      color: Colors.purple,
-      child: const Center(
-        child: Text(
-          'INI HOMEE',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-          ),
+    return Material(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.purple,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'INI HOMEE',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              child: const Text(
+                'Back',
+                style: TextStyle(
+                  color: Colors.purple,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:onboarding/home.dart';
 
 void main() {
@@ -31,7 +32,8 @@ class Onboarding extends StatefulWidget {
 class _OnboardingState extends State<Onboarding>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
-
+  final PageController _pageController = PageController();
+  int currentPage = 0;
   @override
   void initState() {
     super.initState();
@@ -51,26 +53,51 @@ class _OnboardingState extends State<Onboarding>
         elevation: 0,
         backgroundColor: Colors.transparent,
         actions: [
-          TextButton(
-            onPressed: () {
-              _controller.forward();
-            },
-            child: GestureDetector(
-              onTap: () {
-                MaterialPageRoute(builder: (context) => const Home());
-              },
-              child: const Text(
-                'Skip',
-                style: TextStyle(
-                  color: Colors.purple,
-                  fontSize: 18,
+          if (currentPage != 3)
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) {
+                      return const Home();
+                    }),
+                  );
+                },
+                child: const Text(
+                  'Skip',
+                  style: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 18,
+                  ),
                 ),
               ),
             ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        children: const [
+          ContainerBody(
+            child: Screen1(),
+          ),
+          ContainerBody(
+            child: Screen2(),
+          ),
+          ContainerBody(
+            child: Screen3(),
+          ),
+          ContainerBody(
+            child: Screen4(),
           ),
         ],
       ),
-      body: PageView(),
       bottomNavigationBar: SafeArea(
         minimum: const EdgeInsets.all(15),
         child: SizedBox(
@@ -84,7 +111,7 @@ class _OnboardingState extends State<Onboarding>
                     size: const Size(55, 55),
                     child: CircularProgressIndicator(
                       backgroundColor: Colors.transparent,
-                      value: _controller.value,
+                      value: _controller.value + 0.25,
                       valueColor:
                           const AlwaysStoppedAnimation<Color>(Colors.purple),
                       strokeWidth: 2.5,
@@ -96,7 +123,40 @@ class _OnboardingState extends State<Onboarding>
                   right: 0,
                   bottom: 0,
                   top: 0,
-                  child: NextButton(controller: _controller),
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    padding: const EdgeInsets.all(5),
+                    child: GestureDetector(
+                      onTapDown: (_) {
+                        if (currentPage == 3) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Home(),
+                            ),
+                          );
+                        } else {
+                          _pageController.animateToPage(
+                            currentPage + 1,
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeIn,
+                          );
+                          _controller.value += .25;
+                        }
+                      },
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.purple,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -107,36 +167,115 @@ class _OnboardingState extends State<Onboarding>
   }
 }
 
-class NextButton extends StatelessWidget {
-  const NextButton({
+class ContainerBody extends StatelessWidget {
+  final Widget? child;
+  const ContainerBody({
     Key? key,
-    required AnimationController controller,
-  })  : _controller = controller,
-        super(key: key);
-
-  final AnimationController _controller;
+    this.child,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 45,
-      height: 45,
-      padding: const EdgeInsets.all(5),
-      child: GestureDetector(
-        onTapDown: (_) {
-          _controller.value += .25;
-        },
-        child: Container(
-          decoration: const BoxDecoration(
-            color: Colors.purple,
-            shape: BoxShape.circle,
-          ),
-          child: const Icon(
-            Icons.arrow_forward_ios_rounded,
-            color: Colors.white,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+      ),
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      color: Colors.transparent,
+      child: child,
+    );
+  }
+}
+
+class Screen1 extends StatelessWidget {
+  const Screen1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset('assets/library.svg'),
+        const Text(
+          'Browse library and find your favorite books',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
         ),
-      ),
+      ],
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  const Screen2({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text(
+          'Reading is the key to success',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SvgPicture.asset('assets/book.svg'),
+      ],
+    );
+  }
+}
+
+class Screen3 extends StatelessWidget {
+  const Screen3({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset('assets/book1.svg'),
+        const Text(
+          'Many books are waiting for you',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Screen4 extends StatelessWidget {
+  const Screen4({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset('assets/student.svg'),
+        const Text(
+          'The future of the nation is in the hands of the young',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 }
